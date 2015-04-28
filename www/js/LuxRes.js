bb28c78de4de38a8ceb5a2e60ae579389d3b773c
Naasty <naasty04@yahoo.com>
2015-04-28 21:54:46 +0100

    jhj

--- www/js/LuxRes.js
+++ www/js/LuxRes.js
@@ -2,7 +2,9 @@
 function onLoad(){
 $(".luxRes").click(function(event)
 {
-	window.location = event.target.id;
+	//window.location = event.target.id;
+	window.location = "Pictures.html";
 	window.sessionStorage.setItem("place",event.target.innerText);
+
 });
 }
