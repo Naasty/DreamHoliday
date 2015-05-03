1663c3333e716f3556f84a728518cdcce2dff1e9
Naasty <naasty04@yahoo.com>
2015-05-03 22:24:15 +0100

    as

--- www/js/NatParks.js
+++ www/js/NatParks.js
@@ -4,7 +4,11 @@ $(".natParks").click(function(event)
 {
 	window.location = "Pictures.html";
 	window.sessionStorage.setItem("place",event.target.innerText);
-	window.sessionStorage.setItem("site",event.target.id);
-	
+
+	var data=$('#'+event.target.id).data(event.target.id);
+
+	var pos = {"lat":data.lat,"lng":data.lng};
+
+    window.sessionStorage.setItem("position",JSON.stringify(pos));
 });
 }
