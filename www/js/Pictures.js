f5eb86dd8afce7059d5bb70e4174ca608d2768f7
Naasty <naasty04@yahoo.com>
2015-05-04 21:24:37 +0100

    as

--- www/js/Pictures.js
+++ www/js/Pictures.js
@@ -8,9 +8,9 @@ var imageSearch;
 function initialize()
 {
   $.ajaxSetup({ cache: true });
-  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+  $.getScript('https//connect.facebook.net/en_US/sdk.js', function(){
     FB.init({
-      appId: '{your-app-id}',
+      appId: '955385664473150',
       version: 'v2.3' // or v2.0, v2.1, v2.0
     });     
     $('#loginbutton,#feedbutton').removeAttr('disabled');
