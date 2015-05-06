e2656cfd4738b1e53188ef50034b9fde1b72a634
Naasty <naasty04@yahoo.com>
2015-05-06 19:49:06 +0100

    a

--- www/js/NatParks.js
+++ www/js/NatParks.js
@@ -83,8 +83,8 @@ var rad = function(x) {
 
  function getDistance(p1, p2) {
   var R = 6378137; // Earth’s mean radius in meter
-  var dLat = rad(p2.lat - p1.lat;
-  var dLong = rad(p2.lng- p1.lng;
+  var dLat = rad(p2.lat - p1.lat);
+  var dLong = rad(p2.lng- p1.lng);
   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(rad(p1.lat) * Math.cos(rad(p2.lat) *
     Math.sin(dLong / 2) * Math.sin(dLong / 2);
